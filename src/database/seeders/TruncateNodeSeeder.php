18321aa6f1d1de3da9d796be5e72611df258e66e
yerson <yerfe717@gmail.com>
2024-04-16 23:16:50 -0400

    cambios

--- src/database/seeders/TruncateNodeSeeder.php
+++ src/database/seeders/TruncateNodeSeeder.php
@@ -7,9 +7,9 @@ use DB;
 class TruncateNodeSeeder extends Seeder {
         
         public function run(){
-            \yedrick\Master\App\Models\FieldOption::truncate();
             \yedrick\Master\App\Models\Field::truncate();
             \yedrick\Master\App\Models\Node::truncate();
+            \yedrick\Master\App\Models\FieldOption::truncate();
             \yedrick\Master\App\Models\Menu::truncate();
             \yedrick\Master\App\Models\MenuTranslation::truncate();
             \yedrick\Master\App\Models\ImageFolder::truncate();

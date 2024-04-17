39fdad9013cb3c3da021622f95189ad178432cd9
yerson <yerfe717@gmail.com>
2024-04-16 23:21:05 -0400

    cambios

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,9 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::group(['middleware' => 'auth'], function() {
     // Route::get('model-list/{nodeName}', 'MainController@index');

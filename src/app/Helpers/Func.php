f48b10fb29591751d2e7fae6ec6df03e3ea09243
yedrick10 <yerfe717@gmail.com>
2024-06-24 23:18:58 -0400

    add dev

--- src/app/Helpers/Func.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace yedrick\Master\App\Helpers;
-
-class Func {
-
-}
--- src/app/Listeners/CreateNode.php
+++ src/app/Listeners/CreateNode.php
@@ -14,15 +14,6 @@ class CreateNode{
             $saved = true;
         }
         if(!$node->model){
-            // if($node->location=='package'){
-            //     $node->model = '\Solunes\Master\App\\'.str_replace('_','-',studly_case($node->name));
-            // } else if($node->location=='app') {
-            //     $node->model = '\App\\'.str_replace('_','-',studly_case($node->name));
-            // } else if(strpos($node->folder, 'todotix') !== false) {
-            //     $node->model = '\Todotix\\'.ucfirst($node->location).'\App\\'.str_replace('_','-',studly_case($node->name));
-            // } else {
-            //     $node->model = '\Solunes\\'.ucfirst($node->location).'\App\\'.str_replace('_','-',studly_case($node->name));
-            // }
             $node->model = '\App\\Models\\'.str_replace('_','-',Str::studly($node->name));
             $saved = true;
         }
